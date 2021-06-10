8ba80909fc28d511d43e57748bd8232de30c10b4
Kevin Ngo <kevinngo.cal@gmail.com>
2021-06-09 17:20:14 -0700

    Add tests for create-as-web3api step:2 build, deploy, and test

--- docs/__tests__/guides/create-as-web3api.spec.ts
+++ docs/__tests__/guides/create-as-web3api.spec.ts
@@ -1,14 +1,14 @@
-import { runCommand } from "./utils";
+import { runCommand } from './utils';
 
-import * as fs from "fs";
-import * as rimraf from "rimraf";
+import * as fs from 'fs';
+import * as rimraf from 'rimraf';
 
-jest.setTimeout(120000)
+jest.setTimeout(120000);
 
-describe("create-as-web3api", () => {
-  const tmpFolder = __dirname + "/tmp/create-as-web3api/";
-  const projectName = "test-project";
-  const projectFolder = tmpFolder + "/" + projectName;
+describe('create-as-web3api', () => {
+  const tmpFolder = __dirname + '/tmp/create-as-web3api/';
+  const projectName = 'test-project';
+  const projectFolder = tmpFolder + '/' + projectName;
 
   beforeAll(() => {
     if (fs.existsSync(tmpFolder)) {
@@ -22,32 +22,50 @@ describe("create-as-web3api", () => {
     rimraf.sync(tmpFolder);
   });
 
-  it("step 1", async () => {
+  it('step 1: project setup', async () => {
     {
       const { stdout } = await runCommand(
         `npx @web3api/cli create api assemblyscript ${projectName}`,
         tmpFolder
       );
 
-      expect(stdout).toContain("You are ready");
+      expect(stdout).toContain('You are ready');
     }
 
     {
       const { stdout } = await runCommand(
-        "~/.nvm/nvm install && nvm use",
+        '. ~/.nvm/nvm.sh install && nvm use',
         projectFolder
       );
-
-      expect(stdout).toContain("Now using node");
+      expect(stdout).toContain('Now using node');
     }
 
     {
-      const { stdout } = await runCommand(
-        "yarn",
-        projectFolder
-      );
+      const { stdout } = await runCommand('yarn', projectFolder);
 
-      expect(stdout).toContain("Done");
+      expect(stdout).toContain('Resolving packages...');
+    }
+  });
+
+  it('step 2: build, deploy and test', async () => {
+    {
+      const { stdout } = await runCommand('yarn build', projectFolder);
+      expect(stdout).toContain('Compile Web3API');
+    }
+
+    {
+      const { stdout } = await runCommand('yarn test:env:up', projectFolder);
+      expect(stdout).toContain('Starting test environment...');
+    }
+
+    {
+      const { stdout } = await runCommand('yarn deploy', projectFolder);
+      expect(stdout).toContain('Compile Web3API');
+    }
+
+    {
+      const { stdout } = await runCommand('yarn test', projectFolder);
+      expect(stdout).toContain('getData": 569');
     }
   });
 });
