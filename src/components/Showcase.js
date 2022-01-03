35a1f2c417302a676c52cb955e9144a3405125c3
Kevin Ngo <kevinngo.cal@gmail.com>
2022-01-03 11:40:10 -0800

    feature: Add sub-heading below Welcome to Polywrap heading

--- src/components/Showcase.js
+++ src/components/Showcase.js
@@ -1,10 +1,9 @@
 import React from "react";
-import SearchBar from "../theme/SearchBar";
 import { makeStyles } from "@mui/styles";
 import { Box, Container, Grid, Typography, useTheme } from "@mui/material";
 import CardLink from "./CardLink";
-import BackgroundPolywrap from "../../static/img/polywrapper-hero-blurred.png"
-import useThemeContext from '@theme/hooks/useThemeContext';
+import BackgroundPolywrap from "../../static/img/polywrapper-hero-blurred.png";
+import useThemeContext from "@theme/hooks/useThemeContext";
 
 const useStyles = makeStyles(() => ({
   root: {
@@ -17,28 +16,31 @@ const useStyles = makeStyles(() => ({
 const showcaseCards = [
   {
     title: "Get Started",
-    description: "Create a wrapper and learn in detail what makes integrating with Polywrap so special!",
+    description:
+      "Create a wrapper and learn in detail what makes integrating with Polywrap so special!",
     cta: "Get started",
-    link: "/getting-started/what-is-polywrap"
+    link: "/getting-started/what-is-polywrap",
   },
   {
     title: "Guides",
-    description: "Start building! If you want to build your own wrapper, our guides will walk you through.",
+    description:
+      "Start building! Our guides will walk you through how to build your own wrappers or integrate other ones into your dApp",
     cta: "Start building",
-    link: "/guides/create-as-wrapper/project-setup"
+    link: "/guides/create-as-wrapper/project-setup",
   },
   {
     title: "Advanced",
-    description: "Learn about the advanced concepts of Polywrap, including build pipeline and URI redirects.",
+    description:
+      "Learn about the advanced concepts of Polywrap, including build pipeline configuration and URI redirects.",
     cta: "Learn more",
-    link: "/advanced/build-pipeline"
+    link: "/advanced/build-pipeline",
   },
 ];
 
 export default function Showcase() {
   const theme = useTheme();
   const classes = useStyles(theme);
-  const {isDarkTheme} = useThemeContext();
+  const { isDarkTheme } = useThemeContext();
 
   return (
     <Box mt={12} marginBottom={6} position="relative" zIndex={0}>
@@ -62,19 +64,10 @@ export default function Showcase() {
         <Typography variant="h2" component="h1" align="center">
           Welcome to Polywrap!
         </Typography>
-        {/* <Box 
-          sx={{
-            display: "flex",
-            justifyContent: "center",
-            mt: theme.spacing(4),
-            width: "100%",
-            "& .navbar__search": {
-              pl: 0,
-            },
-          }}
-        >
-          <SearchBar />
-        </Box> */}
+        <Typography mt={4} variant="h5" align="center">
+          Polywrap is a development platform that makes it easy to integrate
+          web3 protocols into any application, written in any language.
+        </Typography>
         <Grid container spacing={4} mt={4}>
           {showcaseCards.map((card) => {
             return (
@@ -84,21 +77,23 @@ export default function Showcase() {
                     {card.title}
                   </Typography>
                   <Box mt={1} color={"var(--ifm-heading-color)"}>
-                    <Typography variant="body1">
-                      {card.description}
-                    </Typography>
+                    <Typography variant="body1">{card.description}</Typography>
                   </Box>
                   <Box mt={1}>
-                    <Typography variant="body1" fontWeight="800" className="card-link">
+                    <Typography
+                      variant="body1"
+                      fontWeight="800"
+                      className="card-link"
+                    >
                       {card.cta} &#8250;
                     </Typography>
                   </Box>
                 </CardLink>
               </Grid>
-            )
+            );
           })}
         </Grid>
       </Container>
     </Box>
   );
-} 
+}
